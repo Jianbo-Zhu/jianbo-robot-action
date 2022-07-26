b64b9ccd6ec09caf32dc468b0e5ef09081aa8467
Jianbo-Zhu <lemurzjb@gmail.com>
2022-07-26 15:05:33 +0800

    add test plan

--- doAction.sh
+++ doAction.sh
@@ -2,4 +2,4 @@ pip3 install -r requirements.txt
 
 robot -d ./output $ROBOT_TESTS_DIR
 
-npx railflow -k $RAILFLOW_KEY -url $TESTRAIL_URL -u $RAILFLOW_USERNAME -p $RAILFLOW_PASSWORD -pr $TESTRAIL_PROJECT -r ./output/output.xml -f Robot -sm path -tp TESTRAIL_TEST_PLAN
+npx railflow export -k $RAILFLOW_KEY -url $TESTRAIL_URL -u $RAILFLOW_USERNAME -p $RAILFLOW_PASSWORD -pr $TESTRAIL_PROJECT -r ./output/output.xml -f Robot -sm path -tp $TESTRAIL_TEST_PLAN

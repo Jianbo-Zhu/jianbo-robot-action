a404f2a6dfa1147685950418e81767393ba6bccf
Jianbo-Zhu <lemurzjb@gmail.com>
2022-07-28 09:27:06 +0800

    enable debug

--- doAction.sh
+++ doAction.sh
@@ -1,5 +1,7 @@
+#!/bin/sh -x
 pip3 install -r requirements.txt
 
 robot -d ./output $ROBOT_TESTS_DIR
+
 
 npx railflow export -k $RAILFLOW_KEY -url $TESTRAIL_URL -u $TESTRAIL_USERNAME -p $TESTRAIL_PASSWORD -pr $TESTRAIL_PROJECT -path $ROBOT_TESTS_DIR -r ./output/output.xml -f Robot -sm path -tp $TESTRAIL_TEST_PLAN
